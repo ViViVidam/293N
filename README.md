07c0cb5fc5fe5ac4dabc9249f4da5259a3a6bc9b
Parker Carlson <parkercarlson@169-231-181-12.wireless.ucsb.edu>
2025-05-30 21:10:26 -0700

    sism data

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# 293N
+# 293N -- Comparison of ABR Algorithms 
+Parker Carlson, Wentai Xie, Rithwik Keur
+
+
+### Reproduce Our Results
+
+##### Comparison under various bitrates
+##### Comparison under start up time
+##### Comparison of SSIM and rebuffering performance
+
+1. Download data from Stanford's Puffer experiment for large-scale comparison
+
+`./download_data.sh`
+
+2. Run the jupyter notebook (`graphs.ipynb`) and run all cells. Graphs will be automatically produced.
---
+++ download_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ./data/real_052525
+# data analyzed from May 25, 2025
+wget https://storage.googleapis.com/puffer-data-release/2025-05-24T11_2025-05-25T11/video_sent_2025-05-24T11_2025-05-25T11.csv
+wget https://storage.googleapis.com/puffer-data-release/2025-05-29T11_2025-05-30T11/video_acked_2025-05-29T11_2025-05-30T11.csv 
+wget https://storage.googleapis.com/puffer-data-release/2025-05-29T11_2025-05-30T11/client_buffer_2025-05-29T11_2025-05-30T11.csv
+wget https://storage.googleapis.com/puffer-data-release/2025-05-29T11_2025-05-30T11/video_size_2025-05-29T11_2025-05-30T11.csv
+wget https://storage.googleapis.com/puffer-data-release/2025-05-29T11_2025-05-30T11/ssim_2025-05-29T11_2025-05-30T11.csv
