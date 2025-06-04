ebc37549a3c2a7d64f16feda99ba2ef86efb2fac
Parker Carlson <parkercarlson@dhcp-46-84.cs.ucsb.edu>
2025-06-04 11:31:12 -0700

    readded data

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir ./data/real_0252525
 cd ./data/real_052525
 # data analyzed from May 25, 2025
 wget https://storage.googleapis.com/puffer-data-release/2025-05-24T11_2025-05-25T11/video_sent_2025-05-24T11_2025-05-25T11.csv
